a1780e24d33bef8ed92692fed94e267f744c5410
Caue-Azevedo <cauecarlosdeazevedoguedes@gmail.com>
2024-12-03 17:13:03 -0300

    Add files via upload

---
+++ TESTE 2 - SISTEMA CADASTRO DE LIVROS/Layouts.java
@@ -0,0 +1,16 @@
+System.out.println("|============================================|");
+System.out.println("|--------------------------------------------|");
+
+ // =  = //
+
+
+import java.util.Scanner;
+
+public class Sistema {
+    // = Função para "limpar" a tela do terminal (apenas insere novas linhas) = //
+    public static void limparTela() {
+        System.out.print("\n\n\n\n\n\n\n");
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
