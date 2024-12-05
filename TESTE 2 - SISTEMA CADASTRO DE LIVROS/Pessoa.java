80968ff810921c1e58e7cc8233eb6853b9993944
Caue-Azevedo <cauecarlosdeazevedoguedes@gmail.com>
2024-12-05 15:51:28 -0300

    Update Pessoa.java

--- TESTE 2 - SISTEMA CADASTRO DE LIVROS/Pessoa.java
+++ TESTE 2 - SISTEMA CADASTRO DE LIVROS/Pessoa.java
@@ -1,3 +1,5 @@
+package com.senai;
+
 public class Pessoa {
     private String nome;
     private String sobrenome;
@@ -11,16 +13,8 @@ public class Pessoa {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getSobrenome() {
         return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
     }
 
     public String getNomeCompleto() {
