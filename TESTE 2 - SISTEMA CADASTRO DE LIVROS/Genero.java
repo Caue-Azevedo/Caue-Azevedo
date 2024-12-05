4ec2e53cf3b94ffb0c6422fd59828ac6f6b37438
Caue-Azevedo <cauecarlosdeazevedoguedes@gmail.com>
2024-12-05 15:50:58 -0300

    Update Genero.java

--- TESTE 2 - SISTEMA CADASTRO DE LIVROS/Genero.java
+++ TESTE 2 - SISTEMA CADASTRO DE LIVROS/Genero.java
@@ -1,8 +1,21 @@
-public class Genero {
-    private int codigo;
-    private String nome;
+package com.senai;
 
-    public Genero(int codigo, String nome) {
+public enum Genero {
+    AUTOAJUDA(1, "Autoajuda"),
+    BIOGRAFIAS(2, "Biografias e Histórias Reais"),
+    EDUCACIONAL(3, "Educação e Didáticos"),
+    HISTORIA(4, "História"),
+    HQ_MANGAS(5, "HQs, Mangás e Graphic Novels"),
+    INFANTIL(6, "Infantil"),
+    LITERATURA_FICCAO(7, "Literatura e Ficção"),
+    RELIGIAO(8, "Religião e Espiritualidade"),
+    ROMANCE(9, "Romance"),
+    SUSPENSE(10, "Suspense");
+
+    private final int codigo;
+    private final String nome;
+
+    Genero(int codigo, String nome) {
         this.codigo = codigo;
         this.nome = nome;
     }
